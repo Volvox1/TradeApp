dd9f4b05d385d32160df6775e8c05b6a38a0baa6
Emre Uğur Akpınar <73476042+Volvox1@users.noreply.github.com>
2024-08-19 16:31:23 +0300

    Add files via upload

---
+++ TradeApp/TradeApp/Controllers/TradeAppController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Business.Extensions.Validations;
+using Business.ProductCommands;
+using Entities;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankApp.Controllers
+{
+    [Authorize]
+    [Route("[controller]s")]
+    [ApiController]
+    public class TradeAppController : ControllerBase
+    {
+        private readonly TradeAppDbContext _context;
+        private readonly IMapper _mapper;
+        public TradeAppController(TradeAppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            GetProduct get = new GetProduct(_context,_mapper);
+            var value = get.Handle();
+            return Ok(value);
+
+
+        }
+        [HttpPost]
+        public IActionResult PostProduct([FromBody] PostProduct.PostProductViewModel newProduct)
+        {
+            PostProduct post = new PostProduct(_context);
+            try
+            {
+                post.ViewModel = newProduct;
+                PostValidation validate = new PostValidation();
+                validate.ValidateAndThrow(post);
+                
+                post.Handle();
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+            return Ok();
+
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            
+
+            try
+            {   
+                DeleteProduct delete=new DeleteProduct(_context);
+                DeleteValidation validate = new DeleteValidation();
+               
+               delete.Id=id;
+                 validate.ValidateAndThrow(delete);
+                delete.Delete();
+            
+
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+            
+        }
+    }
+}
---
+++ TradeApp/TradeApp/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Business.UserCommands;
+using Entities.DBContext;
+using Entities.TokenOperations.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TradeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IDBContext _context;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _config;
+        public UserController(IDBContext dBContext,IMapper mapper,IConfiguration configuration)
+        {
+            _config = configuration;
+            _context = dBContext;
+            _mapper = mapper;
+        }
+
+
+
+        [HttpPost]
+        public IActionResult UserCreate([FromBody] CreateUserModel NewUser)
+        {
+            try
+            {
+CreateUserCommand command= new CreateUserCommand(_context,_mapper);
+            command.Model= NewUser;
+            command.Handle();
+            
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+        }
+        [HttpPost("connect/token")]
+        public ActionResult<Token> CreateToken([FromBody] CreateTokenModel login)
+        {
+            try {CreateTokenCommand command = new CreateTokenCommand(_context,_mapper,_config);
+            command.Model= login;
+            var Token=command.Handle();
+            return Token; }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+        [HttpGet("RefreshToken")]
+        public ActionResult<Token> RefreshToken([FromQuery] string token)
+        {
+            try
+            {
+                RefreshTokenCommand command = new RefreshTokenCommand(_config,_context);
+                command.RefreshToken = token;
+                var ResultToken = command.Handle();
+                return ResultToken;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+    }
+
+}
